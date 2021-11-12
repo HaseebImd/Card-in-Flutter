abba66e3cf19bde788214390c2bd91151b507cb5
HaseebImd <haseeb.imdad@yahoo.com>
2021-11-12 21:59:54 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://pbs.twimg.com/media/FBhWG5JWEAYuw3I.jpg",
+                ),
+                radius: 40,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Haseeb Imdad",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text(
+                "FLUTTER-DOC",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  letterSpacing: 4,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child:ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color:Colors.teal,
+                  ),
+                  title: Text(
+                    "+92-321-1011038",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child:ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color:Colors.teal,
+                  ),
+                  title: Text(
+                    "haseebimdad201@gmail.com",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
